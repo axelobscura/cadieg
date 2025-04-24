872016cf95f97dabda1b9ab7008452b9fd375abf
axelinstrumental <aobscura@instrumental.net>
2025-04-24 17:45:17 -0600

    contacto

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -20,7 +20,27 @@ export default function Contacto() {
               <MenuLateral />
             </div>
             <div className="bg-gray-200 h-full p-10">
-              <h1 className="text-4xl uppercase font-light pb-5">Contacto</h1>
+              <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr] gap-2 items-center justify-center h-full">
+                <div>
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <a href="https://www.facebook.com/cadieg" target="_blank" rel="noopener noreferrer">
+                      <Image
+                        src="/contacto.jpg"
+                        alt="Facebook"
+                        width={500}
+                        height={0}
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div>
+                  <h1 className="text-4xl uppercase font-light pb-5">Contacto</h1>
+                  <p className="text-lg">Si tienes alguna pregunta o comentario, no dudes en ponerte en contacto con nosotros.</p>
+                  <p className="text-lg">Puedes enviarnos un correo electrónico a: <a href="mailto:contacto@cadieg.com.mx" className="text-blue-500">contacto@cadieg.com.mx</a></p>
+                  <p className="text-lg">O llamarnos al: <a href="tel:+5259-9999" className="text-blue-500">+52 55 5555 5555</a></p>
+                  <p className="text-lg">También puedes seguirnos en nuestras redes sociales:</p>
+                </div>
+              </div>
               <hr/>
             </div>
           </div>
